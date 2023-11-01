e69f035896e53f976f0275c67e24984d79f64f01
Diksha Takyar <diksha.takyar@gmail.com>
2023-11-01 14:15:54 +0530

    hoisting

--- HoistingAndCallStack/index.js
+++ HoistingAndCallStack/index.js
@@ -1,10 +1,23 @@
 // getName();
-console.log(x);
+console.log(
+  "x value : ",
+  x
+); /**memory was allocated but it has not been initialised
+Hoisting : accessing JS variables/functions without initialising it */
 console.log(getName);
+console.log(
+  "y val : ",
+  y
+); /** this gives us a reference error : accessing a variable from TDZ */
+
 
 var x = 7;
+let y = 12; /**LET and CONST are hoisted but they are in temporal dead zone
+- TDZ : time between : the variable was hoisted till it was initialized
+- to avoid TDZ : initialise it at top.
+- LET and CONST are not attached to window(GLOBAL OBJECT) but are stored in seperate memory
+*/
 
-function getName(){
-    console.log("we just ran!")
+function getName() {
+  console.log("we just ran!");
 }
-
